581eaab39b49b4ba847eb3dcf88e496771822edc
Karina <milkokarina13@gmail.com>
2024-12-30 21:49:32 +0200

    Add controllers

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,6 @@
 const createHttpError = require('http-errors');
 const { User } = require('../models');
+const { default: mongoose } = require('mongoose');
 
 module.exports.createUser = async (req, res, next) => {
   const { body } = req;
@@ -16,22 +17,18 @@ module.exports.createUser = async (req, res, next) => {
     next(error);
   }
 };
-
 module.exports.getUsers = async (req, res, next) => {
   const {
     query: { page = 1, results = 10, sort = '_id' },
   } = req;
-
   // TODO move to middleware
   const limit = results;
   const offset = (page - 1) * limit;
-
   try {
     const foundUsers = await User.find()
       .sort({ [sort]: -1 })
       .skip(offset)
       .limit(limit);
-
     res.status(200).send({
       data: foundUsers,
     });
@@ -39,64 +36,49 @@ module.exports.getUsers = async (req, res, next) => {
     next(error);
   }
 };
-
 module.exports.getUserById = async (req, res, next) => {
   const {
     params: { userId },
   } = req;
-
   try {
-    const foundUsers = await User.findById(userId).lean();
-
-    if (!foundUsers) {
+    const foundUser = await User.findById(userId).lean();
+    if (!foundUser) {
       return next(createHttpError(404, 'User Not Found'));
     }
-
-    res.status(200).send({
-      data: foundUsers,
-    });
+    res.status(200).send({ data: foundUser });
   } catch (error) {
     next(error);
   }
 };
-
 module.exports.updateUserById = async (req, res, next) => {
   const {
     params: { userId },
     body,
   } = req;
-
   try {
     // TODO move to middleware
     if (!mongoose.Types.ObjectId.isValid(userId)) {
       return next(createHttpError(422, 'Invalid User ID format'));
     }
-
     const updatedUser = await User.findByIdAndUpdate(userId, body, {
       new: true,
       runValidators: true,
     });
-
     if (!updatedUser) {
       return next(createHttpError(404, 'User not found'));
     }
-
     res.status(200).send({ data: updatedUser });
   } catch (error) {
     next(error);
   }
 };
-
 module.exports.deleteUserById = async (req, res, next) => {
   const { userId } = req.params;
-
   try {
     const foundUserById = await User.findByIdAndDelete(userId);
-
     if (!foundUserById) {
       return next(createHttpError(404, 'User Not Found'));
     }
-
     res.status(204).end();
   } catch (error) {
     next(error);
@@ -104,5 +86,3 @@ module.exports.deleteUserById = async (req, res, next) => {
 };
 
 module.exports.createUserPost = async (req, res, next) => {};
-
-module.exports.getUserPosts = async (req, res, next) => {};

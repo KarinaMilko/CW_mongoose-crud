a79c2e4767ff8e394a638f1197891291e6ed793b
Karina <milkokarina13@gmail.com>
2024-12-30 21:40:46 +0200

    Add db connection + user model

--- models/index.js
+++ models/index.js
@@ -5,9 +5,6 @@ const config = require('./../configs/mongoDb.json')[env];
 const { host, port, dbName } = config;
 
 mongoose
-  //   .connect('mongodb://localhost:27017/chat')
-  //   .then(() => console.log('Connection to MongoDB is established'))
-  //   .catch((err) => console.log(err));
   .connect(`mongodb://${host}:${port}/${dbName}`)
   .then(() => console.log('Connection to MongoDB is established'))
   .catch((err) => console.log(err));
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const yup = require('yup');
 
-// TODO move yup schema to oll schemas
+// TODO move yup schema to all schemas
 const EMAIL_VALIDATION_SCHEMAS = yup.string().email();
 
 // required
@@ -9,63 +9,60 @@ const EMAIL_VALIDATION_SCHEMAS = yup.string().email();
 // Numbers, Date: min, max
 // Strings: enum, match, minLength, maxLength
 // Custom validators
-const userSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      minLength: 2,
-      maxLength: 50,
-      required: true,
-      match: [
-        /^[A-Z][a-z]+$/,
-        'FirstName must starts  with capital letter, but got a "{VALUE}"',
-      ],
-    },
-    lastName: {
-      type: String,
-      minLength: 2,
-      maxLength: 50,
-      required: true,
-      match: /^[A-Z][a-z]+$/,
-    },
-    email: {
-      type: String,
-      // match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      validate: (v) => EMAIL_VALIDATION_SCHEMAS.isValid(v),
-      required: true,
-    },
-    birthday: {
-      type: Date,
-      max: new Date(),
-    },
-    gender: {
-      type: String,
-      // TODO move to constants
-      enum: ['male', 'female', 'other'],
-    },
-    isMarried: {
-      type: Boolean,
-      default: false,
-    },
-    workExperience: {
-      type: Number,
-      min: 0,
-      max: 100,
-    },
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    minLength: 2,
+    maxLength: 50,
+    required: true,
+    match: [
+      /^[A-Z][a-z]+$/,
+      'FirstName must starts  with capital letter, but got a "{VALUE}" ...',
+    ],
   },
-  { versionKey: false }
-);
+  lastName: {
+    type: String,
+    minLength: 2,
+    maxLength: 50,
+    required: true,
+    match: /^[A-Z][a-z]+$/,
+  },
+  email: {
+    type: String,
+    // match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    validate: (v) => EMAIL_VALIDATION_SCHEMAS.isValid(v),
+    required: true,
+  },
+  birthday: {
+    type: Date,
+    max: new Date(),
+  },
+  gender: {
+    type: String,
+    // TODO move to constants
+    enum: ['male', 'female', 'other'],
+  },
+  isMarried: {
+    type: Boolean,
+    default: false,
+  },
+  workExperience: {
+    type: Number,
+    min: 0,
+    max: 100,
+  },
+});
 
 const User = mongoose.model('User', userSchema);
 
 // User.create({
-//   firstName: 'Asdfg',
+//   firstName: 'Dsfsdfsd',
 //   lastName: 'Qwert',
 //   email: 'qwerty@qwerty',
-//   birthday: '2020-10-10',
-//   workExperience: 1,
+//   birthday: '2024-10-10',
+//   workExperience: -1,
 // })
-//   .then((data) => console.log(data))
-//   .catch((err) => console.log(err));
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
 module.exports = User;
